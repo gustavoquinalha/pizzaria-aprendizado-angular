7e70963cb6ff52f5f6e4cdbcacd6b702fbe990ea
Vinicius Rufine <viniciusrufine@outlook.com>
2016-03-30 13:04:07 -0300

    inicio SPA

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider,$locationProvider){
+	$routeProvider.when('/',{
+		templateUrl: 'views/home.html',
+		controller: 'homeController'
+	})
+	.when('/cardapio', {
+		templateUrl: 'views/cardapio.html',
+		controller: 'cardapioController'
+	})
+	.when('/contato', {
+		templateUrl: 'views/contato.html',
+		controller: 'contatoController'
+	});
+});
+
+app.controller('homeController', function($scope) {
+	$scope.home = 'TESTE HOME';
+});
+
+
+app.controller('cardapioController', function($scope) {
+	$scope.home = 'TESTE CARDAPIO';
+});
